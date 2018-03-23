4aa97b25e1554a2f9f369663ee7e4361b9d81f84
yangyuncai <yyc@yunlaba.com>
2018-03-23 16:20:15 +0800

    test

--- app/models/Users.php
+++ app/models/Users.php
@@ -3,28 +3,10 @@
 namespace Shop\Model;
 
 use Phalcon\Mvc\Model;
-use Phalcon\Validation;
-use Phalcon\Validation\Validator\PresenceOf;
-use Phalcon\Mvc\Model\Validator\Uniqueness;
 
 class Users extends Model
 {
     public function getSource() {
         return "users";
-    }
-
-    public function validation() {
-        $this->validate(
-            new Uniqueness(
-                [
-                    "field"   => "openid",
-                    "message" => "Value of field 'openid' is already present in another record",
-                ]
-            )
-        );
-
-        if ($this->validationHasFailed() === true) {
-            return false;
-        }
     }
 }
