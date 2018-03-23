0cc785c0ef78c9c83b0927871fca9c1091549f2b
yangyuncai <yyc@yunlaba.com>
2018-03-23 08:09:49 +0800

    test

--- app/controllers/open.php
+++ app/controllers/open.php
@@ -3,10 +3,8 @@
 $app->get('/open/session', function () use ($app) {
     $code     = $app->request->getQuery('code');
     $wxconfig = $app->config['wxconfig'];
-    return $app->util->getWxSessionId($app, $wxconfig['appid'], $wxconfig['appsecret'], $code);
+
+    return $wxconfig;
+    // return $app->util->getWxSessionId($app, $wxconfig['appid'], $wxconfig['appsecret'], $code);
 });
-
-
-
-
 
