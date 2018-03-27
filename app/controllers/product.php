38c182d932244185515cbd030f8b60114a89082f
yangyuncai <yyc@yunlaba.com>
2018-03-27 16:00:51 +0800

    add product

--- app/controllers/product.php
+++ app/controllers/product.php
@@ -4,24 +4,18 @@ use Shop\Model\Products;
 $app->post('/product/add', function () use ($app) {
     $params = json_decode($app->request->getRawBody(), true);
 
-    return $params;
+    // return $params;
+    $ar = new Products();
+    $ar->main_img = $params['img_list'][0];
+    foreach($params as $key => $value) {
+        $ar->$key = $value;
+    }
 
-    // $ar = new Products();
-    // $ar->user_id = $id;
-    // $ar->name = $params['name'];
-    // $ar->product_category_id = $params['category'];
-    // $ar->price_unit_id = $params['unit'];
-    // $ar->price = $params['price'];
-    // $ar->shop_id = $shopId;
-    // $ar->pic_url = $params['pic'];
-    // $ar->inventory = $params['sellnum'];
-    // $ar->date = date("Ymd", time());
-
-    // if (!$ar->save()) {
-    //     return $ar->getMessages();
-    // } else {
-    //     return 'ok';
-    // }
+    if ($ar->save()) {
+        return $ar->id;
+    } else {
+        return 0;
+    }
 });
 
 
