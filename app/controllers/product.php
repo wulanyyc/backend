b04c806cb7fba956ba6d02950838688f5cc5e09a
yangyuncai <yyc@yunlaba.com>
2018-03-29 15:30:23 +0800

    test edit product

--- app/controllers/product.php
+++ app/controllers/product.php
@@ -23,6 +23,49 @@ $app->post('/product/add', function () use ($app) {
     }
 });
 
+$app->post('/product/edit', function () use ($app) {
+    $params = json_decode($app->request->getRawBody(), true);
+
+    if ($params['id'] == 0){
+        $ar = new Products();
+        $ar->main_img = $params['img_list'][0];
+        foreach($params as $key => $value) {
+            if (!empty($value)) {
+                if ($key == 'img_list') {
+                    $ar->$key = json_encode($value);
+                } else {
+                    $ar->$key = $value;
+                }
+            }
+        }
+
+        if ($ar->save()) {
+            return $ar->id;
+        } else {
+            return 0;
+        }
+    } else {
+        $ar = Products::findFirst($params['id']);
+        unset($params['id']);
+        $ar->main_img = $params['img_list'][0];
+        foreach($params as $key => $value) {
+            if (!empty($value)) {
+                if ($key == 'img_list') {
+                    $ar->$key = json_encode($value);
+                } else {
+                    $ar->$key = $value;
+                }
+            }
+        }
+
+        if ($ar->save()) {
+            return $ar->id;
+        } else {
+            return 0;
+        }
+    }
+});
+
 
 $app->get('/product/info/{id:\d+}', function ($id) use ($app) {
     $result = Products::findFirst($id);
