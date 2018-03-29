d8cbf50e341348ec10cddd7e34db4ea9f9422635
yangyuncai <yyc@yunlaba.com>
2018-03-29 14:24:10 +0800

    add product support

--- app/controllers/product.php
+++ app/controllers/product.php
@@ -24,29 +24,7 @@ $app->post('/product/add', function () use ($app) {
 });
 
 
-$app->get('/product/list/{id:\d+}', function ($id) use ($app) {
-    $result = Products::find([
-        'conditions' => 'user_id = ' . $id . ' and deleteflag = 0',
-        'order' => 'id desc',
-        'columns' => 'id,product_category_id,price_unit_id,name,price,pic_url,inventory',
-    ]);
-
-    $data = [];
-    foreach($result as $item) {
-        $tmp = [];
-        foreach($item as $k => $v) {
-            $tmp[$k] = $v;
-
-            if ($k == 'product_category_id') {
-                $tmp[$k] = $app->db->fetchOne("select text from product_category where id=" . $v)['text'];
-            }
-
-            if ($k == 'price_unit_id') {
-                $tmp[$k] = $app->db->fetchOne("select text from product_unit where id=" . $v)['text'];
-            }
-        }
-        $data[] = $tmp;
-    }
-
-    return $data;
+$app->get('/product/info/{id:\d+}', function ($id) use ($app) {
+    $result = Products::findFirst(['id' => $id]);
+    return $result;
 });
