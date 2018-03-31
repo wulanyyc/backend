bec83c4101cded3e36647b0f93289b6d4d73fb85
yangyuncai <yyc@yunlaba.com>
2018-03-31 10:24:16 +0800

    fix user card

--- app/controllers/money.php
+++ app/controllers/money.php
@@ -1,10 +1,10 @@
 <?php
-use Shop\Model\UsersCard;
+use Shop\Model\UserCards;
 use Shop\Model\Users;
 
 //用户认证（手机动态登录）
 $app->get('/money/init/{id:\d+}', function ($id) use ($app) {
-    $result = UsersCard::find(['uid' => $id]);
+    $result = UserCards::find(['uid' => $id]);
     $userInfo = Users::findFirst($id);
 
     $arr = $app->util->objectToArray($result);
